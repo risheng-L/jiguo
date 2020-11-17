9d7664bdc9383f13ec76d763a90eb2629cb92e83
risheng-L <2036498266@qq.com>
2020-11-17 20:37:14 +0800

    '第四次'

---
+++ js/kuwan2.js
@@ -0,0 +1,13 @@
+$.ajax({
+    url:'http://192.168.1.64:3000/play/hot',
+    success:function(res){
+        var data=res[0];
+        var dot_tem=doT.template($('#domm').text());
+        $('#list').html(dot_tem(data))
+    },
+    dataType:'json'
+})
+$('.nomain_loading').on('click',function(){
+    $(this).hide();
+    $('.nomain_load').show();
+})
